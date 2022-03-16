c3a620dff6c5d9d5b39260a9156d746258810acc
Daniel <daniel.dac.id@gmail.com>
2022-03-16 17:07:31 +0100

    update

--- functions.js
+++ functions.js
@@ -2,19 +2,28 @@ function loadSlideshow(){
     // Get Slideshow element assigned as id="slideshow"
     const slideshow = document.getElementById("slideshow");
 
-    // Get all secctions added to the slideshow as childrens
-    const sections = slideshow.children;
-
     // Check slideshow has child elements...
-    if( sections.length == 0){
+    if( slideshow.children.length == 0){
         console.log("ERROR: No child elements defined in the slideshow.");
         return;
     }
 
+    // Create sections wrap
+    const sectionsWrap = document.createElement("div");
+    sectionsWrap.setAttribute("id", "slideshow_sections_block");
+    // Move all slideshow child elements to sections wrap
+    while(slideshow.children.length > 0){
+        sectionsWrap.appendChild(slideshow.firstChild);
+    }
+    slideshow.appendChild(sectionsWrap);
+
+    // Get all secctions added to the slideshow as childrens moved to sectionsWrap
+    const sections = sectionsWrap.children;
+
     const dotBlock = document.createElement("div");
     dotBlock.setAttribute("id", "slideshow_dots_block");
     slideshow.appendChild(dotBlock);
-    for (let index = 0; index < sections.length-1; index++) { // sections.length-1 because ignore the dotBlock just added
+    for (let index = 0; index < sections.length; index++) { // sections.length-1 because ignore the dotBlock just added
         sections[index].setAttribute("id", "slideshow_section_"+index);
         sections[index].classList.add("slideshow_section");
 
